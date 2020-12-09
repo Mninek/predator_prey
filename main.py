0bc1195da667ebbdf0b81049d061ff0d66ef5f1e
Mninek <51962912+Mninek@users.noreply.github.com>
2020-12-09 15:26:41 -0500

    prototype of all things moving and eating

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Prey(pygame.sprite.Sprite):
         self.img = pygame.transform.scale(pygame.image.load('sprites/tech.png'), (40,40))
         self.vision = 250
         self.eaten = 0
+        self.ate_sound = pygame.mixer.Sound('sounds/burger.wav')
 
     def get_img(self):
         return self.img
@@ -50,7 +51,9 @@ class Prey(pygame.sprite.Sprite):
 
             #TODO: MAKE LESS RETARDED
             #HAVE 'FINESE' WHEN HE GETS CLOSE TO STEAKS
-            if mx <= 5 and mx >= -5 and my <= 5 and my >= -5:
+
+            #THANK U BALLOD
+            if -5 <= mx <= 5 and -5 <= my <= 5:
                 self.eat_food(foods, cindex)
             if mx != 0 and mx <= 5:
                 mx  = -4
@@ -69,9 +72,77 @@ class Prey(pygame.sprite.Sprite):
         self.eaten += 1
         print("OM NOM NOM NOM MOTHA FUCKA")
         print("total eaten: " + str(self.eaten))
-        print(index)
         foods.pop(index)
-        
+        self.ate_sound.play()
+
+class Predator(pygame.sprite.Sprite):
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.img = pygame.transform.scale(pygame.image.load('sprites/evil eye.png'), (40,30))
+        self.vision = 300
+        self.hunting = False
+        self.hunt_sound = pygame.mixer.Sound('sounds/puma.wav')
+        self.ate_sound = pygame.mixer.Sound('sounds/cutemeow.wav')
+
+    def get_img(self):
+        return self.img
+
+    def get_tup(self):
+        return (self.x, self.y)
+
+    def move(self, mx, my):
+        self.x += mx
+        self.y += my
+
+    def find_prey(self, preys):
+        closest = 10000000
+        cx, cy = 0,0
+        cindex = 0
+        for index, prey in enumerate(preys):
+            tup = prey.get_tup()
+            fx, fy = tup[0], tup[1]
+            #https://stackoverflow.com/questions/5228383/how-do-i-find-the-distance-between-two-points
+            dist = math. sqrt( (self.x - fx)**2 + (self.y - fy)**2 )
+            if dist < closest:
+                closest = dist
+                cx, cy = fx, fy
+                cindex = index
+            index +=1
+            
+        if closest <= self.vision:
+            if not self.hunting:
+                self.hunt()
+            mx = -self.x + cx
+            my = -self.y + cy
+
+            #TODO: MAKE LESS RETARDED
+            #HAVE 'FINESE' WHEN HE GETS CLOSE TO STEAKS
+            if mx <= 5 and mx >= -5 and my <= 5 and my >= -5:
+                self.eat_prey(preys, cindex)
+            if mx != 0 and mx <= 6:
+                mx  = -5
+            else:
+                mx = 5
+            if my != 0 and my <= 6:
+                my = -5
+            else:
+                my = 5
+            self.move(mx, my)
+            
+    def hunt(self):
+        self.img = pygame.transform.scale(pygame.image.load('sprites/devilish.png'), (40,30))
+        self.hunt_sound.play()
+        self.hunting = True 
+
+    def eat_prey(self, preys, index):
+        print("GOODBYE TECH")
+        preys.pop(index)
+        self.img = pygame.transform.scale(pygame.image.load('sprites/evil eye.png'), (40,30))
+        self.ate_sound.play()
+        self.hunting = False
+
         
 class Food(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -84,7 +155,7 @@ class Food(pygame.sprite.Sprite):
 
     def get_tup(self):
         return (self.x, self.y)
-
+    
 def main():
     x = 1200
     y = 800
@@ -111,6 +182,14 @@ def main():
             j = (j+1)*200
             foods.append(Food(i,j))
 
+    #CATS
+    cats = []
+    cats.append(Predator(200,300))
+
+    #music
+    mixer.music.load('sounds/ccmall_trap.wav')
+    mixer.music.set_volume(.05)
+    mixer.music.play(-1)
 
     game = True        
     background = pygame.transform.scale(pygame.image.load('sprites/background.png'), (x,y))
@@ -118,6 +197,9 @@ def main():
 
     lord_sprite = Spritesheet('sprites/the_lord/sprite.png', 'lord')
     lord = lord_sprite.get_sprite(0,0)
+    lord_w = 1000
+    lord_h = 600
+    up = True
     while game:
         #fill surface
         surface.fill(white)
@@ -131,7 +213,21 @@ def main():
         for tech in techs:
             surface.blit(tech.get_img(), tech.get_tup())
             tech.search_foods(foods)
-        surface.blit(lord, (1100,700))
+
+        for cat in cats:
+            surface.blit(cat.get_img(), cat.get_tup())
+            cat.find_prey(techs)
+            
+        surface.blit(lord, (lord_w,lord_h))
+        if up:
+            lord_h -= 10
+            if lord_h == 0:
+                up = False
+        else:
+            lord_h += 10
+            if lord_h == y-200:
+                up = True
+        lord = lord_sprite.get_sprite(0,0)
         window.update()
         clock.tick(15)
     pygame.quit()
--- spritesheet.py
+++ spritesheet.py
@@ -1,15 +1,31 @@
 import pygame
 
+black = (0,0,0)
+
 class Spritesheet:
     def __init__(self, file, kind):
         self.file = file
         self.sprite_sheet = pygame.image.load(self.file).convert()
         self.kind = kind
+        if self.kind == "lord":
+            self.w = 269
+            self.h = 247
+            self.mw = 1345
+            self.mh = 12350
+            self.x = 0
+            self.y = 0
+            self.dance = True
 
     #x, y is basically where in the sprite sheet were are cutting this out of
     def get_sprite(self, x, y):
-        if self.kind == "lord":
-            sprite = pygame.Surface((269, 247))
-            sprite.set_colorkey((85, 255, 33))
-            sprite.blit(self.sprite_sheet, (0,0), (x,y,269,247))
+        if self.dance:
+            sprite = pygame.Surface((self.w, self.h))
+            sprite.set_colorkey(black)
+            sprite.blit(self.sprite_sheet, (0,0), (self.x,self.y,self.w,self.h))
+            self.dance_sprite()
             return sprite
+
+    def dance_sprite(self):
+        self.x = (self.x + self.w) % self.mw
+        self.y = (self.y + self.h) % self.mh
+        
