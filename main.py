9235c2a8e558fadfa65f0412bc9f3f4c9147636b
mninek <51962912+Mninek@users.noreply.github.com>
2020-12-09 01:03:58 -0500

    Added steaks, tech now chases steaks also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 white = (255,255,255)
 black = (0,0,0)
@@ -13,6 +14,8 @@ class Prey(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.img = pygame.transform.scale(pygame.image.load('sprites/tech.png'), (40,40))
+        self.vision = 250
+        self.eaten = 0
 
     def get_img(self):
         return self.img
@@ -20,15 +23,59 @@ class Prey(pygame.sprite.Sprite):
     def get_tup(self):
         return (self.x, self.y)
         
-    def move(self):
-        self.x += random.randint(-1,1)
-        self.y += random.randint(-1,1)
+    def move(self, mx, my):
+        self.x += mx
+        self.y += my
+        
+    def search_foods(self, foods):
+        closest = 10000000
+        cx, cy = 0,0
+        cindex = 0
+        for index, food in enumerate(foods):
+            tup = food.get_tup()
+            fx, fy = tup[0], tup[1]
+            #https://stackoverflow.com/questions/5228383/how-do-i-find-the-distance-between-two-points
+            dist = math. sqrt( (self.x - fx)**2 + (self.y - fy)**2 )
+            if dist < closest:
+                closest = dist
+                cx, cy = fx, fy
+                cindex = index
+            index +=1
+                
+        if closest <= self.vision:
+            mx = -self.x + cx
+            my = -self.y + cy
+
+            #TODO: MAKE LESS RETARDED
+            #HAVE 'FINESE' WHEN HE GETS CLOSE TO STEAKS
+            if mx <= 5 and mx >= -5 and my <= 5 and my >= -5:
+                self.eat_food(foods, cindex)
+            if mx != 0 and mx <= 5:
+                mx  = -4
+            else:
+                mx = 4
+            if my != 0 and my <= 5:
+                my = -4
+            else:
+                my = 4
+            self.move(mx, my)
+        else:
+            #TODO: BETTER RANDOM ALG SO HE MOVES IN ONE DIRECTION FOR SOME TIME
+            self.move(random.randint(-1,1), random.randint(-1,1))
+
+    def eat_food(self, foods, index):
+        self.eaten += 1
+        print("OM NOM NOM NOM MOTHA FUCKA")
+        print("total eaten: " + str(self.eaten))
+        print(index)
+        foods.pop(index)
+        
         
 class Food(pygame.sprite.Sprite):
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.img = pygame.transform.scale(pygame.image.load('sprites/tech.png'), (20,20))
+        self.img = pygame.transform.scale(pygame.image.load('sprites/beef.png'), (30,30))
 
     def get_img(self):
         return self.img
@@ -48,11 +95,23 @@ def main():
     
     clock = pygame.time.Clock()
     techs = []
+    techs.append(Prey(600,450))
+
+    foods = []
+    for i in range(3):
+        i = (i+1)*200
+        for j in range(3):
+            j = (j+1)*200
+            foods.append(Food(i,j))
+
+            
+    """
     for i in range(3):
         i = (i+1)*200
         for j in range(3):
             j = (j+1)*200
             techs.append(Prey(i,j))
+            """
     background = pygame.transform.scale(pygame.image.load('sprites/background.png'), (x,y))
     font = pygame.font.SysFont("timesnewroman",20)
     while game:
@@ -63,9 +122,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game = False
+        for food in foods:
+            surface.blit(food.get_img(), food.get_tup())
         for tech in techs:
             surface.blit(tech.get_img(), tech.get_tup())
-            tech.move()
+            tech.search_foods(foods)
+        
         window.update()
         clock.tick(15)
     pygame.quit()
